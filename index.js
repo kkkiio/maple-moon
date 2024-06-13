bd3d744ce1b3d5f903e5e9292818f8e1fb1d184f
kkkiio <xc130d210@outlook.com>
2024-06-13 23:33:38 +0800

    feat: try background

--- index.js
+++ index.js
@@ -1,68 +1,285 @@
-"use strict";
 const vertexShaderSource = `#version 300 es
 precision highp float;
-// an attribute is an input (in) to a vertex shader.
-// It will receive data from a buffer
-in vec2 a_position;
-in vec2 a_texcoord;
+in vec4 coord;
+in vec4 color;
 // Used to pass in the resolution of the canvas
-uniform vec2 u_resolution;
-// a varying to pass the texture coordinates to the fragment shader
-out vec2 v_texcoord;
+uniform vec2 screensize;
+out vec2 texpos;
+out vec4 colormod;
 void main(void)
 {
-  // convert the position from pixels to 0.0 to 1.0
-  vec2 zeroToOne = a_position / u_resolution;
-
-  // convert from 0->1 to 0->2
-  vec2 zeroToTwo = zeroToOne * 2.0;
-
-  // convert from 0->2 to -1->+1 (clipspace)
-  vec2 clipSpace = zeroToTwo - 1.0;
-
-  gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+  // screen resolution is 1366 * 768
+  float x = (-1.0 + coord.x * (screensize.y / 384.0) / screensize.x);
+  // Set image to left top
+  float y = 1.0 - coord.y / 384.0;
+  gl_Position = vec4(x, y, 0.0, 1.0);
   // Pass the texcoord to the fragment shader.
-  v_texcoord = a_texcoord;
+  texpos = coord.zw;
+  colormod = color;
 }
 `;
 const fragmentShaderSource = `#version 300 es
 precision highp float;
 // Passed in from the vertex shader.
-in vec2 v_texcoord;
+in vec2 texpos;
+in vec4 colormod;
 // The texture.
 uniform sampler2D u_texture;
+uniform vec2 atlassize;
 out vec4 FragColor;
 
 void main(void)
 {
-  FragColor = texture(u_texture, v_texcoord);
+  FragColor = texture(u_texture, texpos / atlassize) * colormod;
 }
 `;
+/**
+ * Wrapped logging function.
+ * @param {string} msg The message to log.
+ */
+function error(msg) {
+  if (topWindow.console) {
+    if (topWindow.console.error) {
+      topWindow.console.error(msg);
+    } else if (topWindow.console.log) {
+      topWindow.console.log(msg);
+    }
+  }
+}
+const defaultShaderType = ["VERTEX_SHADER", "FRAGMENT_SHADER"];
+/**
+ * Creates a program from 2 sources.
+ *
+ * @param {WebGLRenderingContext} gl The WebGLRenderingContext
+ *        to use.
+ * @param {string[]} shaderSourcess Array of sources for the
+ *        shaders. The first is assumed to be the vertex shader,
+ *        the second the fragment shader.
+ * @param {string[]} [opt_attribs] An array of attribs names. Locations will be assigned by index if not passed in
+ * @param {number[]} [opt_locations] The locations for the. A parallel array to opt_attribs letting you assign locations.
+ * @param {module:webgl-utils.ErrorCallback} opt_errorCallback callback for errors. By default it just prints an error to the console
+ *        on error. If you want something else pass an callback. It's passed an error message.
+ * @return {WebGLProgram} The created program.
+ * @memberOf module:webgl-utils
+ */
+function createProgramFromSources(
+  gl,
+  shaderSources,
+  opt_attribs,
+  opt_locations,
+  opt_errorCallback
+) {
+  const shaders = [];
+  for (let ii = 0; ii < shaderSources.length; ++ii) {
+    shaders.push(
+      loadShader(
+        gl,
+        shaderSources[ii],
+        gl[defaultShaderType[ii]],
+        opt_errorCallback
+      )
+    );
+  }
+  return createProgram(
+    gl,
+    shaders,
+    opt_attribs,
+    opt_locations,
+    opt_errorCallback
+  );
+}
+/**
+ * Creates a program, attaches shaders, binds attrib locations, links the
+ * program and calls useProgram.
+ * @param {WebGLShader[]} shaders The shaders to attach
+ * @param {string[]} [opt_attribs] An array of attribs names. Locations will be assigned by index if not passed in
+ * @param {number[]} [opt_locations] The locations for the. A parallel array to opt_attribs letting you assign locations.
+ * @param {module:webgl-utils.ErrorCallback} opt_errorCallback callback for errors. By default it just prints an error to the console
+ *        on error. If you want something else pass an callback. It's passed an error message.
+ * @memberOf module:webgl-utils
+ */
+function createProgram(
+  gl,
+  shaders,
+  opt_attribs,
+  opt_locations,
+  opt_errorCallback
+) {
+  const errFn = opt_errorCallback || error;
+  const program = gl.createProgram();
+  shaders.forEach(function (shader) {
+    gl.attachShader(program, shader);
+  });
+  if (opt_attribs) {
+    opt_attribs.forEach(function (attrib, ndx) {
+      gl.bindAttribLocation(
+        program,
+        opt_locations ? opt_locations[ndx] : ndx,
+        attrib
+      );
+    });
+  }
+  gl.linkProgram(program);
+
+  // Check the link status
+  const linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (!linked) {
+    // something went wrong with the link
+    const lastError = gl.getProgramInfoLog(program);
+    errFn(
+      `Error in program linking: ${lastError}\n${shaders
+        .map((shader) => {
+          const src = addLineNumbersWithError(gl.getShaderSource(shader));
+          const type = gl.getShaderParameter(shader, gl.SHADER_TYPE);
+          return `${glEnumToString(gl, type)}:\n${src}`;
+        })
+        .join("\n")}`
+    );
+
+    gl.deleteProgram(program);
+    return null;
+  }
+  return program;
+}
+/**
+ * Loads a shader.
+ * @param {WebGLRenderingContext} gl The WebGLRenderingContext to use.
+ * @param {string} shaderSource The shader source.
+ * @param {number} shaderType The type of shader.
+ * @param {module:webgl-utils.ErrorCallback} opt_errorCallback callback for errors.
+ * @return {WebGLShader} The created shader.
+ */
+function loadShader(gl, shaderSource, shaderType, opt_errorCallback) {
+  const errFn = opt_errorCallback || error;
+  // Create the shader object
+  const shader = gl.createShader(shaderType);
+
+  // Load the shader source
+  gl.shaderSource(shader, shaderSource);
+
+  // Compile the shader
+  gl.compileShader(shader);
+
+  // Check the compile status
+  const compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (!compiled) {
+    // Something went wrong during compilation; get the error
+    const lastError = gl.getShaderInfoLog(shader);
+    errFn(
+      `Error compiling shader: ${lastError}\n${addLineNumbersWithError(
+        shaderSource,
+        lastError
+      )}`
+    );
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+class ResourceLoader {
+  constructor() {
+    this.nxJson = null;
+  }
+  async loadNxJson(path) {
+    const response = await fetch(path);
+    const json = await response.json();
+    this.nxJson = json;
+  }
+  load(nodepath) {
+    let parts = nodepath.split("/");
+    return parts.reduce((acc, part) => {
+      if (part in acc) return acc[part];
+      throw new Error(
+        `resolve resource failed, path=${nodepath}, missing part=${part}`
+      );
+    }, this.nxJson);
+  }
+}
+
+const bidRegex = /(\d+)\.png/;
+
+class SpriteLoader {
+  constructor(i) {
+    this.i = i;
+    this.bitmaps = {};
+  }
+  loadSprite(imageFilename) {
+    // Asynchronously load an image
+    const imageLoad = new Promise((r) => {
+      const img = new Image();
+      img.onload = () => r(img);
+      img.src = imageFilename;
+    });
+    const jsonFilename =
+      imageFilename.substring(0, imageFilename.lastIndexOf(".")) + ".json";
+    const spriteJsonFetch = fetch(jsonFilename).then((response) =>
+      response.json()
+    );
+    return Promise.all([imageLoad, spriteJsonFetch]).then(([img, json]) =>
+      this.onLoad(img, json)
+    );
+  }
+  onLoad(img, json) {
+    const canvas = document.createElement("canvas");
+    canvas.width = json.meta.size.w;
+    canvas.height = json.meta.size.h;
+    const ctx = canvas.getContext("2d", {
+      willReadFrequently: true,
+    });
+    ctx.drawImage(img, 0, 0);
+
+    for (const key in json.frames) {
+      const frame = json.frames[key].frame;
+      const bid = bidRegex.exec(key)[1];
+      const data = ctx.getImageData(frame.x, frame.y, frame.w, frame.h);
+      this.bitmaps[bid] = {
+        id: this.i * 100000 + bid,
+        data,
+        w: frame.w,
+        h: frame.h,
+      };
+    }
+  }
+  loadBitmap(bid) {
+    const bmp = this.bitmaps[bid];
+    if (!bmp) {
+      throw new Error(`Bitmap ${bid} not found`);
+    }
+    console.log("loading bitmap", this.i, bid);
+    return bmp;
+  }
+}
+
+const VWIDTH = 1366;
+const VHEIGHT = 768;
+const ATLASW = 8192;
+const ATLASH = 8192;
 function main() {
   const canvas = document.getElementById("canvas");
   const gl = canvas.getContext("webgl2");
   if (!gl) {
     throw new Error("WebGL not supported");
   }
-  const VWIDTH = 1366;
-  const VHEIGHT = 768;
 
   canvas.width = VWIDTH;
   canvas.height = VHEIGHT;
   // Set image to left top, multiplied by phone (screensize y / 384), screen resolution is 1366 * 768
-  const program = webglUtils.createProgramFromSources(gl, [
+  const program = createProgramFromSources(gl, [
     vertexShaderSource,
     fragmentShaderSource,
   ]);
 
   // look up where the vertex data needs to go.
-  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  const texcoordAttributeLocation = gl.getAttribLocation(program, "a_texcoord");
+  const coordAttributeLocation = gl.getAttribLocation(program, "coord");
+  const colorAttributeLocation = gl.getAttribLocation(program, "color");
   // look up uniform locations
-  const resolutionUniformLocation = gl.getUniformLocation(
+  const screensizeUniformLocation = gl.getUniformLocation(
     program,
-    "u_resolution"
+    "screensize"
   );
+  const atlassizeUniformLocation = gl.getUniformLocation(program, "atlassize");
 
   // Vertex Buffer Object
   const VBO = gl.createBuffer();
@@ -71,26 +288,27 @@ function main() {
   // and make it the one we're currently working with
   gl.bindVertexArray(vao);
   // Turn on the attribute
-  gl.enableVertexAttribArray(positionAttributeLocation);
-  gl.enableVertexAttribArray(texcoordAttributeLocation);
+  gl.enableVertexAttribArray(coordAttributeLocation);
+  gl.enableVertexAttribArray(colorAttributeLocation);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
-  const bytes_of_vertex = 2 * 2 + 2 * 4;
+  const bytesOfCoord = 4 * 2;
+  const bytesOfVertex = bytesOfCoord + 4 * 4;
   gl.vertexAttribPointer(
-    positionAttributeLocation,
-    2,
+    coordAttributeLocation,
+    4,
     gl.SHORT,
     false,
-    bytes_of_vertex,
+    bytesOfVertex,
     0
   );
   gl.vertexAttribPointer(
-    texcoordAttributeLocation,
-    2,
+    colorAttributeLocation,
+    4,
     gl.FLOAT,
     false,
-    bytes_of_vertex,
-    4
+    bytesOfVertex,
+    bytesOfCoord
   );
 
   // Create a texture.
@@ -99,41 +317,49 @@ function main() {
   gl.activeTexture(gl.TEXTURE0 + 0);
   // bind to the TEXTURE_2D bind point of texture unit 0
   gl.bindTexture(gl.TEXTURE_2D, texture);
-  // Fill the texture with a 1x1 blue pixel.
+  // create the atlas buffer
   gl.texImage2D(
     gl.TEXTURE_2D,
     0,
     gl.RGBA,
-    1,
-    1,
+    ATLASW,
+    ATLASH,
     0,
     gl.RGBA,
     gl.UNSIGNED_BYTE,
-    new Uint8Array([0, 0, 255, 255])
+    null
   );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
   //   gl.bindVertexArray(null);
 
-  // Asynchronously load an image
-  const cursor_img = new Image();
-  cursor_img.src = "nx/files/raw/Basic.img/Cursor/0/0.png";
-  cursor_img.addEventListener("load", function () {
-    // Now that the image has loaded make copy it to the texture.
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(
-      gl.TEXTURE_2D,
-      0,
-      gl.RGBA,
-      gl.RGBA,
-      gl.UNSIGNED_BYTE,
-      cursor_img
-    );
-    gl.generateMipmap(gl.TEXTURE_2D);
-  });
+  const prepareResourcePromises = [];
+  const uiLoader = new ResourceLoader();
+  prepareResourcePromises.push(
+    uiLoader.loadNxJson("nx/files/raw/UI.nx/nx.json")
+  );
+  const uiSpriteLoader = new SpriteLoader(0);
+  uiLoader.spriteLoader = uiSpriteLoader;
+  prepareResourcePromises.push(
+    uiSpriteLoader.loadSprite("nx/files/raw/UI.nx/spritesheet.png")
+  );
+
+  const mapLoader = new ResourceLoader();
+  prepareResourcePromises.push(
+    mapLoader.loadNxJson("nx/files/raw/Map.nx/nx.json")
+  );
+  const mapSpriteLoader = new SpriteLoader(1);
+  mapLoader.spriteLoader = mapSpriteLoader;
+  prepareResourcePromises.push(
+    mapSpriteLoader.loadSprite("nx/files/raw/Map.nx/spritesheet.png")
+  );
+
+  const soundLoader = new ResourceLoader();
+  prepareResourcePromises.push(
+    soundLoader.loadNxJson("nx/files/raw/Sound.nx/nx.json")
+  );
 
   let requestAnimationFrameId = null;
-  let lastTime = 0;
-  let dropCounter = 0;
-  let dropInterval = 10;
 
   let halt = false;
   let game_win = null;
@@ -149,6 +375,8 @@ function main() {
   let keyup_left = null;
   let keyup_right = null;
   let onmousemove = null;
+  let onmousedown = null;
+  let onmouseup = null;
 
   // window.addEventListener("keydown", (e) => {
   //   if (halt && e.keyCode === 32) {
@@ -206,41 +434,18 @@ function main() {
   //   }
   // });
   canvas.onmousemove = (evt) => {
-    // var rect = canvas.getBoundingClientRect();
-    // const x = evt.clientX - rect.left;
-    // const y = evt.clientY - rect.top;
     const x = evt.offsetX;
-    // const y = VHEIGHT - evt.offsetY;
     const y = evt.offsetY;
-    onmousemove(x, y);
+    if (onmousemove) onmousemove(x, y);
+  };
+  canvas.onmousedown = (evt) => {
+    if (onmousedown) onmousedown();
+  };
+  canvas.onmouseup = (evt) => {
+    if (onmouseup) onmouseup();
   };
 
   const importObject = {
-    // canvas: {
-    //   render_box: (a, b, c, d) => {
-    //     context.strokeStyle = "#FF0000";
-    //     context.strokeRect(a, b, c, d);
-    //   },
-    //   render3: (img, sx, sy) => {
-    //     context.drawImage(img, sx, sy);
-    //   },
-    //   render: (img, sx, sy, sw, sh, dx, dy, dw, dh) => {
-    //     context.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh);
-    //   },
-    //   draw: (
-    //     g,
-    //     bitmap,
-    //     rect_left,
-    //     rect_top,
-    //     rect_right,
-    //     rect_bottom,
-    //     color_r,
-    //     color_g,
-    //     color_b,
-    //     color_a,
-    //     angle
-    //   ) => {},
-    // },
     gl: {
       flush: (triangles) => {
         // Tell WebGL how to convert from clip space to pixels
@@ -257,21 +462,36 @@ function main() {
         // Pass in the canvas resolution so we can convert from
         // pixels to clipspace in the shader
         gl.uniform2f(
-          resolutionUniformLocation,
+          screensizeUniformLocation,
           gl.canvas.width,
           gl.canvas.height
         );
+        gl.uniform2f(atlassizeUniformLocation, ATLASW, ATLASH);
 
         gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
         gl.bufferData(gl.ARRAY_BUFFER, triangles, gl.STREAM_DRAW);
-        gl.drawArrays(gl.TRIANGLES, 0, triangles.byteLength / bytes_of_vertex);
+        gl.drawArrays(gl.TRIANGLES, 0, triangles.byteLength / bytesOfVertex);
 
         // gl.bindVertexArray(null);
+      },
+      texSubImage2D: (x, y, bmp) => {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texSubImage2D(
+          gl.TEXTURE_2D,
+          0,
+          x,
+          y,
+          bmp.w,
+          bmp.h,
+          gl.RGBA,
+          gl.UNSIGNED_BYTE,
+          bmp.data
+        );
       },
     },
     vertices: {
       new: (length) => {
-        return new ArrayBuffer(length * bytes_of_vertex);
+        return new ArrayBuffer(length * bytesOfVertex);
       },
       set: (
         buffer,
@@ -279,35 +499,41 @@ function main() {
         localcoord_x,
         localcoord_y,
         texcoord_x,
-        texcoord_y
-        // color_r,
-        // color_g,
-        // color_b,
-        // color_a
+        texcoord_y,
+        color_r,
+        color_g,
+        color_b,
+        color_a
       ) => {
-        const positions = new Int16Array(buffer, i * bytes_of_vertex);
+        const positions = new Int16Array(buffer, i * bytesOfVertex);
         positions[0] = localcoord_x;
         positions[1] = localcoord_y;
-        const texcoords = new Float32Array(buffer, i * bytes_of_vertex + 2 * 2);
-        texcoords[0] = texcoord_x;
-        texcoords[1] = texcoord_y;
+        positions[2] = texcoord_x;
+        positions[3] = texcoord_y;
+        const colors = new Float32Array(
+          buffer,
+          i * bytesOfVertex + bytesOfCoord
+        );
+        colors[0] = color_r;
+        colors[1] = color_g;
+        colors[2] = color_b;
+        colors[3] = color_a;
       },
     },
     resource: {
-      load_idle_cursor: () => {
-        return cursor_img;
-        // return {
-        //   width: 32,
-        //   height: 32,
-        // };
-      },
+      ui_loader: () => uiLoader,
+      map_loader: () => mapLoader,
+      sound_loader: () => soundLoader,
+      load_bitmap: (loader, bid) => loader.spriteLoader.loadBitmap(bid),
     },
     bitmap: {
-      width: (bmp) => {
-        return bmp.width;
-      },
-      height: (bmp) => {
-        return bmp.height;
+      id: (bmp) => bmp.id,
+      width: (bmp) => bmp.w,
+      height: (bmp) => bmp.h,
+    },
+    time: {
+      now_micro: () => {
+        return performance.now() * 1000;
       },
     },
     spectest: {
@@ -318,39 +544,23 @@ function main() {
   };
 
   function update(time = 0) {
-    game_update(time);
+    game_update(time * 1000);
     if (!halt) {
       requestAnimationFrameId = requestAnimationFrame(update);
     }
   }
 
-  function start() {
-    Object.assign(globalThis, importObject);
-    //   WebAssembly.instantiateStreaming(
-    //     fetch("target/wasm-gc/release/build/lib/lib.wasm"),
-    //     importObject
-    //   ).then((obj) => {
-    //     obj.instance.exports._start();
-    //     // game_win = obj.instance.exports["mario/main::game_win"];
-    //     // game_lose = obj.instance.exports["mario/main::game_lose"];
-    //     // game_update = obj.instance.exports["game_update"];
-    //     // keydown_up = obj.instance.exports["mario/main::keydown_up"];
-    //     // keydown_down = obj.instance.exports["mario/main::keydown_down"];
-    //     // keydown_left = obj.instance.exports["mario/main::keydown_left"];
-    //     // keydown_right = obj.instance.exports["mario/main::keydown_right"];
-    //     // keydown_B = obj.instance.exports["mario/main::keydown_B"];
-    //     // keyup_up = obj.instance.exports["mario/main::keyup_up"];
-    //     // keyup_down = obj.instance.exports["mario/main::keyup_down"];
-    //     // keyup_left = obj.instance.exports["mario/main::keyup_left"];
-    //     // keyup_right = obj.instance.exports["mario/main::keyup_right"];
-    // requestAnimationFrameId = requestAnimationFrame(update);
-    //   });
-    import("./target/js/release/build/lib/lib.js").then((m) => {
+  Object.assign(globalThis, importObject);
+  Promise.all(prepareResourcePromises)
+    .then(() => import("./target/js/release/build/lib/lib.js"))
+    .then((m) => {
       game_update = m.game_update;
       onmousemove = m.onmousemove;
+      onmousedown = m.onmousedown;
+      onmouseup = m.onmouseup;
+      globalThis.transit_to = m.transit_to;
+      m.game_start();
       requestAnimationFrameId = requestAnimationFrame(update);
     });
-  }
-  start();
 }
 main();
